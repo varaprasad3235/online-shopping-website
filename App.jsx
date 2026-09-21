f7e39a916ca662990cf17b584da7207b66042f78
varaprasad3235 <153343361+varaprasad3235@users.noreply.github.com>
2026-09-21 15:55:14 +0530

    src

--- App.jsx
+++ App.jsx
@@ -1,45 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Add from './pages/Add'
-import List from './pages/List'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
-import Login from './components/Login'
-import { ToastContainer } from 'react-toastify';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
-export const currency = '₹'
+import Verify from './pages/Verify'
 
 const App = () => {
-
-  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
-
-  useEffect(()=>{
-    localStorage.setItem('token',token)
-  },[token])
-
   return (
-    <div className='bg-gray-50 min-h-screen'>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <ToastContainer />
-      {token === ""
-        ? <Login setToken={setToken} />
-        : <>
-          <Navbar setToken={setToken} />
-          <hr />
-          <div className='flex w-full'>
-            <Sidebar />
-            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
-              <Routes>
-                <Route path='/add' element={<Add token={token} />} />
-                <Route path='/list' element={<List token={token} />} />
-                <Route path='/orders' element={<Orders token={token} />} />
-              </Routes>
-            </div>
-          </div>
-        </>
-      }
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/verify' element={<Verify />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
